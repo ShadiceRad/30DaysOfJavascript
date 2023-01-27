dd1cd0b633aa2632f9526597cd4bab074cc09a23
Shadice <shadice.maxwell@gmail.com>
2023-01-27 17:25:41 -0500

    update question

--- daytwo.js
+++ daytwo.js
@@ -154,11 +154,18 @@ console.log(bcuz.match(/because/gi).length)
 
 //Clean the following text and find the most frequent word (hint, use replace and regular expressions).
     const sentences = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-    const clean = sentences.replace(/[^a-zA-Z ]/g, "")
+    let clean = sentences.replace(/[^a-zA-Z ]/g, "")
     let k = clean.split(' ')
-
-
-    console.log(k)
+    console.log(clean)
+    for (let c =0; c <= k.length; c++ )
+{
+    var y = k[c]
+    var re = new RegExp(y, 'g');
+    
+    console.log(clean.match(re))
+    
+    //console.log(clean.match(y.length))
+}
 
 //Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 
