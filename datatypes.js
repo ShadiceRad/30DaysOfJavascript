801b7184449804ab5bb8e442d38718a928432a4c
Shadice <shadice.maxwell@gmail.com>
2023-01-26 20:47:58 -0500

    First Commit

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+console.log(typeof null)
+
+const a = value
+const b = value
+const c = value
+const d = value
+
+const e = 5
+const f = 10
+const g = 15
+const h = 20
+
+
---
+++ intro.js
@@ -0,0 +1,11 @@
+// Declaring variables with number values
+let age = 100 // age in years
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
+
+//Comments can make code readable
+//Welcome to 30DaysofJavascript
+/*Comments can make code readable,
+easy to reuse and informative */
